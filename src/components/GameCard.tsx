7dce2001347a3a9c32c8599e8352d72084bd7bb9
Leopold <czy.leopold97@gmail.com>
2024-07-11 17:08:17 +1000

    Get cropped images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { game } from '../Entity/Game'
 import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
 import PlatFormIconList from './PlatFormIconList';
 import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
     game: game;
@@ -10,7 +11,7 @@ interface Props {
 const GameCard = ({game}: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image}/>
+      <Image src={getCroppedImageUrl(game.background_image)}/>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justify="space-between">
