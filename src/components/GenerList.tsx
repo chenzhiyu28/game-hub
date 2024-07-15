132d8338fb45481f6f937d0b6014c8ba97505fed
Leopold <czy.leopold97@gmail.com>
2024-07-15 21:35:09 +1000

    Clean up the genre

--- src/components/GenerList.tsx
+++ src/components/GenerList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 import { Genre } from '../Entity/Genre';
@@ -16,13 +16,14 @@ const GenerList = ({onSelectGenre}: Props) => {
     <>
     {isLoading? <Spinner />: null}
     {error === " "? null: <Text>error: {error}</Text>}
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
       {data.map(genre => 
       <ListItem key={genre.id} paddingY={2} onClick={() => {onSelectGenre(genre); setID(genre.id)}}>
         <HStack>
-            <Image boxSize="32px" borderRadius={8} 
+            <Image boxSize="32px" borderRadius={8} objectFit="cover"
               src={getCroppedImageUrl({imageUrl: genre.image_background})}/>
-            <Button fontWeight={selectedGenreID === genre.id? "bold": "normal"} 
+            <Button whiteSpace="normal" textAlign="left" fontWeight={selectedGenreID === genre.id? "bold": "normal"} 
             fontSize="large" variant="link">{genre.name}
             </Button>
         </HStack>
