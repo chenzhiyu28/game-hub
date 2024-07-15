a46d48707bf171496425494de31ae926c2a8db79
Leopold <czy.leopold97@gmail.com>
2024-07-15 22:05:56 +1000

    Ship genres into static local data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,11 @@
 import { Genre } from "../Entity/Genre";
 import useData from "./useData";
+import genres from "../data/genres";
 
 
 const useGenres = () => useData<Genre>("/genres")
+const useGenres2 = () => ({
+    data: genres, isLoading: false, error: null
+})
 
-export default useGenres;
+export default useGenres2;
